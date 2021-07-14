2e15ae03dd8d9bfff6782241eebd0994385cf1eb
MrTango <md@derico.de>
2021-07-14 22:39:32 +0300

    Back to development: 1.0a5

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = "\n\n".join(
 
 setup(
     name="plone.pdfexport",
-    version="1.0a4",
+    version="1.0a5.dev0",
     description="PDF export of Plone content based on the weasyprint library.",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
