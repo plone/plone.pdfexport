8430d973485991703fb5afb7fcef805b522c19d7
MrTango <md@derico.de>
2021-10-06 11:53:26 +0300

    Preparing release 1.0

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,8 +2,8 @@ Changelog
 =========
 
 
-1.0 (unreleased)
-------------------
+1.0 (2021-10-06)
+----------------
 
 - Improve German translations
   [MrTango]
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = "\n\n".join(
 
 setup(
     name="plone.pdfexport",
-    version="1.0b2.dev0",
+    version="1.0",
     description="PDF export of Plone content based on the weasyprint library.",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
