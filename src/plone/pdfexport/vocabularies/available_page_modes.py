2038fb0f97df216632d394db8b081a1a3907586e
MrTango <md@derico.de>
2021-10-26 18:41:01 +0300

    Add default_mode, portrait_css, landscape_css to controlpanel

--- src/plone/pdfexport/vocabularies/available_page_modes.py
+++ src/plone/pdfexport/vocabularies/available_page_modes.py
@@ -24,8 +24,8 @@ class AvailablePageModes(object):
         # Just an example list of content for our vocabulary,
         # this can be any static or dynamic data, a catalog result for example.
         items = [
-            VocabItem(u"sony-a7r-iii", _(u"Sony Aplha 7R III")),
-            VocabItem(u"canon-5d-iv", _(u"Canon 5D IV")),
+            VocabItem(u"landscape", _(u"Landscape")),
+            VocabItem(u"portrait", _(u"Portrait")),
         ]
 
         # Fix context if you are using the vocabulary in DataGridField.
