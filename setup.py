dcd2c8bc8f52efba3642ddffa71e1ffd774e0f98
MrTango <md@derico.de>
2022-02-23 16:21:58 +0200

    require Py >=3.7 and remove restapi down pinning

--- setup.py
+++ setup.py
@@ -46,13 +46,13 @@ setup(
     package_dir={"": "src"},
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*,!=3.6.*",
+    python_requires=">=3.7",
     install_requires=[
         "setuptools",
         # -*- Extra requirements: -*-
         "z3c.jbot",
         "plone.api>=1.8.4",
-        "plone.restapi < 8.0.0",
+        "plone.restapi",
         "plone.app.dexterity",
         "weasyprint",
         "beautifulsoup4",
