e2ba76fd2aa1b491fa78bf5e34a29affc4093c14
MrTango <md@derico.de>
2024-07-17 11:32:59 +0300

    implement resolve_reg_image, include banner in pdf_export and fix resolving img

--- src/plone/pdfexport/views/resolve_reg_image.py
+++ src/plone/pdfexport/views/resolve_reg_image.py
@@ -1,17 +1,33 @@
 # -*- coding: utf-8 -*-
 
+from AccessControl.ZopeGuards import guarded_getattr
+from plone.formwidget.namedfile.converter import b64decode_file
+from plone.namedfile.browser import DisplayFile
+from plone.namedfile.file import NamedImage
+from plone.registry.interfaces import IRegistry
+from zope.component import getMultiAdapter, getUtility
+from zope.publisher.interfaces import NotFound
+
 from plone.pdfexport import _
-from Products.Five.browser import BrowserView
-
-# from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone.pdfexport.controlpanels.pdf_export import IPdfExportControlPanel
 
 
-class ResolveRegImage(BrowserView):
-    # If you want to define a template here, please remove the template from
-    # the configure.zcml registration of this view.
-    # template = ViewPageTemplateFile('resolve_reg_image.pt')
+class ResolveRegImage(DisplayFile):
 
-    def __call__(self):
-        # Implement your own actions:
-        self.msg = _(u'A small message')
-        return self.index()
+    def __init__(self, context, request):
+        super(ResolveRegImage, self).__init__(context, request)
+        self.filename = None
+        self.data = None
+        registry = getUtility(IRegistry)
+        settings = registry.forInterface(
+            IPdfExportControlPanel,
+            prefix="pdfexport",
+        )
+        if getattr(settings, 'banner', False):
+            filename, data = b64decode_file(settings.banner)
+            data = NamedImage(data=data, filename=filename)
+            self.data = data
+            self.filename = filename
+
+    def _getFile(self):
+        return self.data
